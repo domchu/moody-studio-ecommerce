0665067de5c9fbc73e0c74b8eb219be0917785d2
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-08-31 00:05:16 +0100

    chore: added the discount price

--- src/components/BedSheet.jsx
+++ src/components/BedSheet.jsx
@@ -8,7 +8,18 @@ function BedSheet() {
       <div className="bedsheet__container__text">
         <h2>BEDSHEET SETS</h2>
         <div className="price">
-          <h2 style={{ color: "#9d5b05", margin: "1rem 0" }}>$50:00</h2>
+          <div className="price__amount">
+            <h2 style={{ color: "#9d5b05", margin: "1rem 0" }}>$50:00</h2>
+            <h2
+              style={{
+                color: "#9d5b05",
+                margin: "1rem 3rem",
+                textDecoration: "line-through",
+              }}
+            >
+              $250:00
+            </h2>
+          </div>
           <div className="bedsheet__text">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
