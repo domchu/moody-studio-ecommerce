fc836da574f39ee220143e9474b0486849d8073c
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-05 23:13:55 +0100

    Chore:added the path to all the links

--- src/components/HandleNavlink.jsx
+++ src/components/HandleNavlink.jsx
@@ -6,7 +6,13 @@ import React from "react";
 const HandleNavlink = () => {
   return (
     <main>
-      <Route exact path="/" component={Home} />
+      <switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/Product" component={Store} />
+        <Route path="/Footer" component={Contact} />
+        <Route path="/ Newsletter" component={News} />
+        <Route path="/TopRate" component={Brand} />
+      </switch>
     </main>
   );
 };
